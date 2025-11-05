2a1e4a6ee88b23d5052fee07703372f6daf4ef70
vijay810 <vijay951mane@gail.com>
2025-11-05 17:02:27 +0530

    update

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Carousel } from 'bootstrap';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent  implements OnInit {
-
-  constructor() { }
+export class DashboardComponent implements OnInit, AfterViewInit {
+  constructor() {}
 
   ngOnInit() {}
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      const carouselEl = document.querySelector('#carouselExampleAutoplaying');
+      if (carouselEl) {
+        new Carousel(carouselEl, {
+          interval: 3000, 
+          ride: 'carousel', 
+          pause: false, 
+          wrap: true, 
+        });
+      }
+    }, 500);
+  }
 }
