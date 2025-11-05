934adec9347ad4617f7dabe146f619c7d92a6f1e
vijay810 <vijay951mane@gail.com>
2025-11-05 17:43:05 +0530

    update

--- src/app/components/layoutpage/layoutpage.component.ts
+++ src/app/components/layoutpage/layoutpage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { IonContent } from "@ionic/angular/standalone";
 import { HeaderComponent } from "../header/header.component";
@@ -14,7 +14,21 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./layoutpage.component.scss'],
 })
 export class LayoutpageComponent implements OnInit {
+  @ViewChild(IonContent, { static: false }) content!: IonContent;
+  showScrollButton = false;
+
   constructor() {}
 
   ngOnInit() {}
+
+  // Detect scroll
+  onScroll(event: any) {
+    const scrollTop = event.detail.scrollTop;
+    this.showScrollButton = scrollTop > 10;
+  }
+
+  // Scroll to top smoothly
+  async scrollToTop() {
+    await this.content.scrollToTop(500); // 500ms smooth scroll
+  }
 }
